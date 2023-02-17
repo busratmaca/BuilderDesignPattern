045b0496b5e1e0a3f69a64abe7e0a22c8b54c763
busratmaca <busra.yilmazatmaca@gmail.com>
2023-02-17 23:31:59 +0300

    main commit

---
+++ DesignPatterns.BuilderDesignPattern/DesignPatterns.BuilderDesignPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesignPatterns.BuilderDesignPattern;
+
+var urlBuilder = new URLBuilder("https://localhost");
+
+urlBuilder
+    .Append("api")
+    .Append("v1")
+    .Append("product")
+    .QueryStringAppend("id", "5")
+    .QueryStringAppend("name", "book");
+
+var finalUrl = urlBuilder.BuildURL();
+
+Console.WriteLine("URL: "+ finalUrl);
+
---
+++ DesignPatterns.BuilderDesignPattern/DesignPatterns.BuilderDesignPattern/URLBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace DesignPatterns.BuilderDesignPattern
+{
+    public class URLBuilder
+    {
+        private readonly StringBuilder urlBuilder = new();
+        private readonly StringBuilder queryStringParams = new();
+
+        const char delimeter = '/';
+
+        public string BaseURL { get; set; }
+
+        public URLBuilder(string baseUrl)
+        {
+            BaseURL = baseUrl;
+        }
+
+        public URLBuilder Append(string value)
+        {
+            //localhost/api/v1/product
+            urlBuilder.Append(value);
+            urlBuilder.Append(delimeter);
+            return this;
+
+        }
+
+        public URLBuilder QueryStringAppend(string name, string value)
+        {
+            //localhost/api/v1/product?id=5&name=test
+            queryStringParams.AppendFormat("{0}={1}&", name, value);
+            return this;
+
+        }
+
+        public string BuildURL()
+        {
+            if (BaseURL.EndsWith(delimeter))
+                urlBuilder.Insert(0, BaseURL);
+            else
+                urlBuilder.Insert(0, BaseURL + delimeter);
+
+            //localhost/api/v1/product
+            var url = urlBuilder.ToString().TrimEnd('&');
+
+            if (queryStringParams.Length > 0)
+            {
+                string qsParams = queryStringParams.ToString().TrimEnd('&');
+                url = urlBuilder.ToString().TrimEnd(delimeter).TrimEnd('?');
+
+                url = $"{url}?{qsParams}";
+
+            }
+        
+            return url.TrimEnd(delimeter);
+        }
+    }
+}
+
