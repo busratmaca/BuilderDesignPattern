978ae02f24bc25e4cec9a1cbf99a35965ddef56b
busratmaca <busra.yilmazatmaca@gmail.com>
2023-02-19 15:24:10 +0300

    null check

--- DesignPatterns.BuilderDesignPattern/DesignPatterns.BuilderDesignPattern/URLBuilderv2/Extension.cs
+++ DesignPatterns.BuilderDesignPattern/DesignPatterns.BuilderDesignPattern/URLBuilderv2/Extension.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 namespace DesignPatterns.BuilderDesignPattern.URLBuilderv2
 {
-	public static class Extension
-	{
+    public static class Extension
+    {
         public static string addDelimeter(this string str, char delimeter)
         {
-            
+
             return $"{str}{delimeter}";
         }
 
         public static string Concat(this URL url)
         {
-            return $"{url.protocol}{url.hostname}{url.pathParam}?{url.queryParam}";
+            if (url.queryParam is null)
+                return $"{url.protocol}{url.hostname}{url.pathParam}";
+            else
+                return $"{url.protocol}{url.hostname}{url.pathParam}?{url.queryParam}";
         }
     }
 }
